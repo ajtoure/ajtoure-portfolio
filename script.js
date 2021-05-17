d6843a0cfc5972e9bfa22ead5d07d539ca019c41
AJ <assiyahjamilla@gmail.com>
2021-05-17 00:45:49 -0400

    nearly done, small details and some js remaining

---
+++ script.js
@@ -0,0 +1,14 @@
+//The menu, contact form and scroll will be animated
+
+//scroll animation/effect for each section
+
+
+//menu animation here
+//first the menu (already only displaying the hamburger) will be queried
+//this will switch the class to a class that allows it to show the full menu
+
+
+//contact form here interactions here
+//this will toggle the contact form open and closed
+//the two classes will be toggled when the arrow is clicked
+
