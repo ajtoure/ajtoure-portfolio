bbc080c616fc73665b0df32341f73cd94494a29d
AJ <assiyahjamilla@gmail.com>
2021-06-14 11:39:39 -0400

    changed font back, cleaned up layout, added some flex properties to aid with overflow

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+const portfolioApp = {};
 //The menu, contact form and scroll will be animated
 
 //scroll animation/effect for each section
-
 
 //menu animation here
 //first the menu (already only displaying the hamburger) will be queried
@@ -12,3 +12,14 @@
 //this will toggle the contact form open and closed
 //the two classes will be toggled when the arrow is clicked
 
+portfolioApp.init = function(){
+   $(window).scroll(function(){
+      const winTop = $(window).scrollTop(), 
+      docHeight = $(document).height(), 
+      winHeight = $(window).height();
+      const scrolled = (winTop/(docHeight - winHeight))*100;
+      $('.scrollLine').css('width', (scrolled + '%'));
+   });
+};
+
+portfolioApp.init();
